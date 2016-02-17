4b8ab465636136eae464cdaa378b0df1e6f90395
Aaron Filson <aaronfilson@gmail.com>
2016-02-17 00:13:56 -0800

    Remove the now ignored directory node_modules

---
+++ AaronFilson/app/js/client.js
@@ -0,0 +1,45 @@
+const angular = require('angular');
+
+const activityApp = angular.module('activityApp', []);
+
+activityApp.controller('activityController', ['$scope', '$http', ($scope, $http) => {
+  $scope.activity = [];
+
+  $http.get('http://localhost:3050/api/activity')
+    .then((res) => {
+      console.log('success!');
+      $scope.activity = res.data;
+    }, (err) => {
+      console.log(err);
+    });
+
+  $scope.createActivity = function(act) {
+    $http.post('http://localhost:3050/api/activity', act)
+      .then((res) => {
+        $scope.activity.push(res.data);
+        $scope.newAct = null;
+      }, (err) => {
+        console.log(err);
+      });
+  };
+
+  $scope.deleteActivity = function(act) {
+    $http.delete('http://localhost:3050/api/activity/' + act._id)
+      .then((res) => {
+        $scope.activity = $scope.activity.filter((i) => i !== act);
+      }, (err) => {
+        console.log(err)
+      });
+  };
+
+  $scope.updateActivity = function(act) {
+    $http.put('http://localhost:3050/api/activity/' + act._id, act)
+      .then((res) => {
+        $scope.activity[$scope.activity.indexOf(act)] = act;
+        act.editting = false;
+      }, (err) => {
+        console.log(err);
+        act.editting = false;
+      });
+  };
+}]);
