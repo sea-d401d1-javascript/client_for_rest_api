32cb68a8246c042640e45d1aa0a2248f7561c53e
Yueyue07 <xww.qyy@gmail.com>
2016-02-22 10:59:35 -0800

    add resource service for retrieving and updating data from server and db

---
+++ yueyueqin/app/js/services/resources_service.js
@@ -0,0 +1,42 @@
+var handleSuccess = function(callback) {
+  return function(res) {
+    callback(null,res.data);
+  };
+};
+
+var handleFailure = function(callback) {
+  return function(res) {
+    callback(res);
+  };
+};
+
+module.exports = exports = function(app) {
+  app.factory('twoResource',['$http', function($http) {
+    var Resource = function(ResourceName){
+      this.ResourceName = ResourceName;
+    };
+
+    Resource.prototype.getAll = function(callback){
+      $http.get('http://localhost:3000/api' + this.ResourceName)
+        .then(handleSuccess(callback), handleFailure(callback));
+    };
+
+    Resource.prototype.create = function(data,callback) {
+      $http.post('http://localhost:3000/api' + this.ResourceName, data)
+       .then(handleSuccess(callback), handleFailure(callback));
+    };
+
+    Resource.prototype.update = function(data, callback) {
+      $http.put('http://localhost:3000/api' + this.ResourceName + '/' + data._id, data)
+        .then(handleSuccess(callback), handleFailure(callback));
+    };
+
+    Resource.prototype.delete = function(data, callback) {
+      $http.delete('http://localhost:3000/api' + this.ResourceName + '/' + data._id)
+        .then(handleSuccess(callback), handleFailure(callback));
+    };
+    return function(ResourceName) {
+      return new Resource(ResourceName);
+    };
+  }]);
+}
