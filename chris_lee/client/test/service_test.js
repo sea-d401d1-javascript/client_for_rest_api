2828b9d0bb9d523664d0ecdc2a285286bc71a833
Christopher Lee <yensen@gmail.com>
2016-02-22 19:33:46 -0800

    all service tests working

--- chris_lee/client/test/service_test.js
+++ chris_lee/client/test/service_test.js
@@ -27,20 +27,18 @@ describe('resource service', () => {
   });
 
   it('should handle success of the get function', () => {
-    var testCT = {name: 'test CT'};
     var err, res;
-    $httpBackend.expectGET('http://localhost:3000/api/test').respond(200, testCT);
+    $httpBackend.expectGET('http://localhost:3000/api/test').respond(200, {name: 'test CT'});
     testService.get((_err_, _res_) => {
       err = _err_;
       res = _res_;
     });
     $httpBackend.flush();
     expect(err).toBe(null);
-    expect(res.name).toBe(testCT.name);
+    expect(res.name).toBe('test CT');
   });
 
-  it('should handle failure of the getAll function', () => {
-    var testCT = {name: 'test CT'};
+  it('should handle failure of the get function', () => {
     var err, res;
     $httpBackend.expectGET('http://localhost:3000/api/test').respond(404, 'error');
     testService.get((_err_, _res_) => {
@@ -54,4 +52,88 @@ describe('resource service', () => {
     expect(res).toBe(undefined);
   });
 
-})
+  it('should handle success of the create function', () => {
+    var newCT = {name: 'new CT'};
+    var err, res;
+    $httpBackend.expectPOST('http://localhost:3000/api/test').respond(200, newCT);
+    testService.create(newCT, (_err_, _res_) => {
+      err = _err_;
+      res = _res_;
+    });
+    $httpBackend.flush();
+    expect(err).toBe(null);
+    expect(res.name).toBe(newCT.name);
+  });
+
+  it('should handle failure of the create function', () => {
+    var newCT = {name: 'new CT'};
+    var err, res;
+    $httpBackend.expectPOST('http://localhost:3000/api/test').respond(404, 'error');
+    testService.create(newCT, (_err_, _res_) => {
+      err = _err_;
+      res = _res_;
+    });
+    $httpBackend.flush();
+    expect(err).not.toBe(null);
+    expect(err.status).toBe(404);
+    expect(err.data).toBe('error');
+    expect(res).toBe(undefined);
+  });
+
+  it('should handle success of the update function', () => {
+    var testCT = {name: 'test CT', _id: '123'};
+    var err, res;
+    $httpBackend.expectPUT('http://localhost:3000/api/test/123').respond(200, testCT);
+    testService.update(testCT, (_err_, _res_) => {
+      err = _err_;
+      res = _res_;
+    });
+    $httpBackend.flush();
+    expect(err).toBe(null);
+    expect(res.name).toBe(testCT.name);
+  });
+
+  it('should handle failure of the update function', () => {
+    var testCT = {name: 'test CT', _id: '123'};
+    var err, res;
+    $httpBackend.expectPUT('http://localhost:3000/api/test/123').respond(404, 'error');
+    testService.update(testCT, (_err_, _res_) => {
+      err = _err_;
+      res = _res_;
+    });
+    $httpBackend.flush();
+    expect(err).not.toBe(null);
+    expect(err.status).toBe(404);
+    expect(err.data).toBe('error');
+    expect(res).toBe(undefined);
+  });
+
+  it('should handle success of the delete function', () => {
+    var testCT = {name: 'test CT', _id: '123'};
+    var err, res;
+    $httpBackend.expectDELETE('http://localhost:3000/api/test/123').respond(200, testCT);
+    testService.delete(testCT, (_err_, _res_) => {
+      err = _err_;
+      res = _res_;
+    });
+    $httpBackend.flush();
+    expect(err).toBe(null);
+    expect(res.name).toBe(testCT.name);
+  });
+
+  it('should handle failure of the delete function', () => {
+    var testCT = {name: 'test CT', _id: '123'};
+    var err, res;
+    $httpBackend.expectDELETE('http://localhost:3000/api/test/123').respond(404, 'error');
+    testService.delete(testCT, (_err_, _res_) => {
+      err = _err_;
+      res = _res_;
+    });
+    $httpBackend.flush();
+    expect(err).not.toBe(null);
+    expect(err.status).toBe(404);
+    expect(err.data).toBe('error');
+    expect(res).toBe(undefined);
+  });
+
+});
