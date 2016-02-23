dc185724c296d59c32b16bd70e6b54586263a845
jamesvermillion <jamesvermillion@yahoo.com>
2016-02-23 10:54:29 -0800

    css base layer

--- jimVermillionFE/app/js/client.js
+++ jimVermillionFE/app/js/client.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('!style!css!../css/style.css');
+//require('!style!css!../css/style.css');
 const angular = require('angular');
 var flowerApp = angular.module('flower', []);
 require('./services/resource_services')(flowerApp);
