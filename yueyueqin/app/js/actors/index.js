7cd510722f15be3c769359002bbaba08f92573b4
Yueyue07 <xww.qyy@gmail.com>
2016-02-28 20:10:51 -0800

    add directives for actors and movies

--- yueyueqin/app/js/actors/index.js
+++ yueyueqin/app/js/actors/index.js
@@ -1,3 +1,5 @@
 module.exports = function(app) {
   require('./controllers/actors_controller')(app);
+  // require('./directives/actor_display_directive')(app);
+  // require('./directives/actor_form_directive')(app);
 };
--- yueyueqin/app/js/movies/index.js
+++ yueyueqin/app/js/movies/index.js
@@ -1,3 +1,5 @@
 module.exports = function(app) {
   require('./controllers/movies_controller')(app);
+  // require('./directives/movie_display_directive')(app);
+  // require('./directives/movie_form_directive')(app);
 };
