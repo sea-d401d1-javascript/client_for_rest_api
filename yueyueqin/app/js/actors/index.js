8b3c809ab407830c8118b06f0dce59a4e80d1746
Yueyue07 <xww.qyy@gmail.com>
2016-02-28 21:05:02 -0800

    add actors edit list directive

--- yueyueqin/app/js/actors/index.js
+++ yueyueqin/app/js/actors/index.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
   require('./controllers/actors_controller')(app);
-  // require('./directives/actor_display_directive')(app);
+  require('./directives/actor_edit_directive')(app);
   // require('./directives/actor_form_directive')(app);
 };
