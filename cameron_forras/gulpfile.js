675788702e67e0278c6fb3eb6df5b88a0402c56f
Cameron Forras <cameronforras@hotmail.com>
2016-03-03 10:20:42 -0800

    converted css to scss

--- cameron_forras/gulpfile.js
+++ cameron_forras/gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const babel = require('babel-loader');
 const html = require('html-loader');
+const sass = require('gulp-sass');
+const maps = require('gulp-sourcemaps');
+const minifyCss = require('gulp-minify-css');
 
 gulp.task('html:dev', () => {
   gulp.src(__dirname + '/app/**/*.html')
@@ -41,6 +44,14 @@ gulp.task('webpack:test', () => {
     .pipe(gulp.dest('test/'));
 });
 
+gulp.task('sass:dev', function() {
+  gulp.src(__dirname + '/app/**/*.scss')
+  .pipe(maps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(minifyCss())
+  .pipe(maps.write('./'))
+  .pipe(gulp.dest('./build'));
+});
 
-gulp.task('build:dev', ['webpack:dev', 'html:dev', 'css:dev']);
+gulp.task('build:dev', ['webpack:dev', 'html:dev', 'css:dev', 'sass:dev']);
 gulp.task('default', ['build:dev']);
